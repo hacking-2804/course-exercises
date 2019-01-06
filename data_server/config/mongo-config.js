3523ebefe65ce1997f42d771f8a7a6cd7a9464ae
Eric Bellamy <ericbellamy@rogers.com>
2019-01-05 20:04:49 -0500

    progress

---
+++ data_server/config/mongo-config.js
@@ -0,0 +1,37 @@
+require('dotenv').config({ path: require('find-config')('.env') })
+let model = require('../models/model.js');
+
+// Database
+
+var mongoose = require('mongoose');
+Promise = require('bluebird');
+mongoose.Promise = Promise;
+var ObjectId = mongoose.Types.ObjectId;
+
+var options = {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
+    useNewUrlParser: true,
+};
+console.log(options);
+  
+var mongooseConnectionString = 'mongodb://' + process.env.MONGO_IP + ":" + process.env.MONGO_PORT + '/' + process.env.MONGO_DBNAME +'?authSource=' + process.env.MONGO_AUTHSOURCE;
+mongoose.connect(mongooseConnectionString,options);
+
+function find(){
+    console.log("FINDING...");
+    model.find({}, function(err, result){
+        console.log("MONGO CONFIG :: THE RESULTS OF THE FIND");
+        console.log(err);
+        console.log(result);
+    });
+}
+
+function update(){
+
+}
+
+module.exports = {
+    find: find,
+    update: update
+};
