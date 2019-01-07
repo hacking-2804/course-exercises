b8946c397aad01fe94447c0b15aaf20c5ba92c80
Forest Anderson <forestkzanderson@gmail.com>
2019-01-06 22:56:41 -0500

    Added Docker Mongodb and Node

---
+++ web_server/install.sh
@@ -0,0 +1,10 @@
+npm i dotenv
+npm i express
+npm i cookie-session
+npm i connect-flash
+npm i serve-favicon
+npm i ejs
+npm i cookie-parser
+npm i request-ip
+npm i socket.io-client
+mv node_modules ../
